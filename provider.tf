6d9aa7bad02265e103525611c785a50480bb3bcd
kanthmc <lkmc468@gmail.com>
2023-08-07 15:47:46 +0530

    EKS

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.66.1"
+    }
+  }
+}
+
+provider "aws" {
+  access_key = "AKIA2IH2S3VGSCYZPIGZ"
+  secret_key = "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"
+  region = "ap-southeast-1"
+}
